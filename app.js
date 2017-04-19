6b2fb5584d5a42da36aea028961b3fe4d355c9a4
daxapps <daxapps777@gmail.com>
2017-04-19 15:12:44 -0500

    Update scores and Change active player

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -36,5 +36,36 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	diceDOM.src = 'dice-' + dice + '.png';
 
 	// 3. Update the round score IF the rolled number was NOT a 1
+	if (dice !== 1) {
+		// add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		document.querySelector('.dice').style.display = 'none';
+	}
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
